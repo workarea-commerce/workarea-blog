d399fee5a9026165b7901264fcff5ea30f0dba96
Tom Scott <tscott@weblinc.com>
2019-09-26 13:25:59 -0400

    Fix Content Search Text for Blog Entries
    Since workarea-commerce/workarea-content-search#1 seems to be on the
    back burner for now, and regardless of this blogs will have to handle
    their search summary text on their own anyway. Update
    `Search::Storefront::Content` to use the blog entry summary if it
    exists, falling back to whatever the content-search gem uses to generate
    its text. At the moment, this involves extracting text from content
    blocks that have more than 5 words in them, but this may change in the
    next major.
    
    (#6)

---
+++ test/models/workarea/search/storefront/blog_content_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+module Workarea
+  if Plugin.installed?(:content_search)
+    module Search
+      class Storefront
+        class BlogContentTest < Workarea::TestCase
+          include TestCase::SearchIndexing
+
+          setup :setup_models
+
+          def setup_models
+            @entry = create_blog_entry(blog: create_blog)
+            @content = Workarea::Content.for(@entry)
+            @content.blocks.create!(
+              type: :html,
+              data: { 'html' => '<p>lorem ipsum dolor sit amet</p</p>' }
+            )
+          end
+
+          def test_use_summary_for_text_when_available
+            @entry.update!(summary: 'foo bar baz')
+            indexed = Content.new(@content)
+
+            assert_equal('foo bar baz', indexed.text)
+          end
+
+          def test_fall_back_to_default_block_text_extraction
+            indexed = Content.new(@content)
+
+            assert_equal('lorem ipsum dolor sit amet', indexed.text)
+          end
+        end
+      end
+    end
+  end
+end
